2d3664c3ad935b1c029504c23c9a5bf75b6307de
Harsh1652 <86983684+Harsh1652@users.noreply.github.com>
2025-05-15 00:38:51 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -539,7 +539,7 @@ export default async (req, res) => {
   // Add these headers to your response
 res.setHeader('Access-Control-Allow-Credentials', true);
 res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,POST');
 res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
 // Handle OPTIONS request (preflight)
